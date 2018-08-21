d81d8433e585a95d1442ea508ebcdc33c54e98e2
Marek Židek <marekzidek@centrum.cz>
2018-08-21 11:33:41 +0200

    fixed minor bug

--- similarity.py
+++ similarity.py
@@ -131,12 +131,12 @@ def w_interval(note_a, note_b):
 		return REST
 
 	# Given that we represent notes as modulo to tonic
-	if note_a.scale == major:
+	if note_a.scale == "major":
 		degree_a = MAJOR_DEG[note_a.pitch % 12]
 		debree_b = MAJOR_DEG[note_b.pitch % 12]
-	elif note_b.scale == minor:
-		degree_a = MINOR_DEG[(note_a.pitch + 9) % 12]
-		degree_b = MINOR_DEG[(note_b.pitch + 9) % 12]
+	elif note_b.scale == "minor":
+		degree_a = MINOR_DEG[(note_a.pitch - 9) % 12]
+		degree_b = MINOR_DEG[(note_b.pitch - 9) % 12]
 	else:
 		return TON[math.abs(note_a.pitch - note_b.pitch)]
 
